994c9b69bd94d4afe0660ace0526e6dbfeb6e29c
maroze <grigorevaalla15@gmail.com>
2021-11-11 11:20:10 +0300

    переделано

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace рпм2
     }
     interface ICosdaet : IDolsnoct
     {
-        Student GetCosdaetSt(string name, string dolj);
+        Student GetCosdaetSt(string name, string grup);
         Propodavatel GetCosdaetPr(string name, string dolj);
     }
     interface ILekcee : IDolsnoct
@@ -27,14 +27,15 @@ namespace рпм2
     {
         string GetZayavlen();
     }
-    public class Kadrovik : ICosdaet // реализация интерфейса кадровка
+    /// <summary>
+    /// реализация интерфейса кадровка
+    /// </summary>
+    public class Kadrovik : ICosdaet
     {
         string name;
-        string dolj;
-        public Kadrovik(string x, string y)
+        public Kadrovik(string x)
         {
             this.name = x;
-            this.dolj = y;
         }
         public string GetFio()
         {
@@ -42,18 +43,18 @@ namespace рпм2
         }
         public string GetDol()
         {
-            return dolj;
+            return "Кадровик";
         }
-        public Student GetCosdaetSt(string name, string dolj)
+        public Student GetCosdaetSt(string nameS, string grup)
         {
-            return new Student(name, dolj);
+            return new Student(nameS, grup);
         }
-        public Propodavatel GetCosdaetPr(string name, string dolj)
+        public Propodavatel GetCosdaetPr(string namePr, string dolj)
         {
-            return new Propodavatel(name, dolj);
+            return new Propodavatel(namePr, dolj);
         }
     }
-    public class Propodavatel : ILekcee // реализация интерфейса преподавателя
+    public class Propodavatel : ILekcee 
     {
         string name;
         string dolj;
@@ -79,7 +80,7 @@ namespace рпм2
             return name + " " + dolj;
         }
     }
-    public class Student : IZayavlen // реализация интерфейса студента
+    public class Student : IZayavlen 
     {
         string name;
         string grup;
@@ -109,18 +110,24 @@ namespace рпм2
     {
         static void Main(string[] args)
         {
-            Kadrovik one = new Kadrovik("К.Е.В.", "кадровик");
+            Kadrovik one = new Kadrovik("К.Е.В.");
             Console.WriteLine(one.GetFio());
             Console.WriteLine(one.GetDol());
-            Console.WriteLine(one.GetCosdaetSt("Н.И.Э.", "3-1п9"));
-            Console.WriteLine(one.GetCosdaetPr("П.З.А.", "преподаватель"));
-            Console.WriteLine("    ");
-            Propodavatel two = new Propodavatel("Н.М.Х.", "преподаватель");
+            string nameS = "Н.З.Ш.";
+            string grup = "3-1п9";
+            Student four = one.GetCosdaetSt(nameS, grup);
+            Console.WriteLine(four.GetFio());
+            Console.WriteLine(four.GetGropp());
+            string namePr = "Р.О.Г.";
+            string dolj = "преподаватель";
+            Propodavatel five = one.GetCosdaetPr(namePr, dolj);
+            Console.WriteLine(five.GetFio());
+            Console.WriteLine(five.GetDol());
+            Propodavatel two = new Propodavatel("А.Х.Г.", "преподаватель");
             Console.WriteLine(two.GetFio());
             Console.WriteLine(two.GetDol());
-            Console.WriteLine(two.GetLekcee());
-            Console.WriteLine("    ");
-            Student three = new Student("Р.З.А.", "3-1п9");
+            Console.WriteLine(two.GetLekcee() + "\n");
+            Student three = new Student("П.Г.А.", "3-19");
             Console.WriteLine(three.GetFio());
             Console.WriteLine(three.GetGropp());
             Console.WriteLine(three.GetZayavlen());
